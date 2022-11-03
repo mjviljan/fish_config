c1bb2cb0ff852ce1e10bc235d942ea6dd10382ae
Mika Viljanen <mika.viljanen@wunderdog.fi>
2022-11-03 09:55:14 +0200

    Add eighths to Rectangle (config and a shell script to set them if config import doesn't work)

---
+++ rectangle-eighths.sh
@@ -0,0 +1,4 @@
+defaults write com.knollsoft.Rectangle topLeftEighth -dict-add keyCode -float 16 modifierFlags -float 1835008
+defaults write com.knollsoft.Rectangle topRightEighth -dict-add keyCode -float 35 modifierFlags -float 1835008
+defaults write com.knollsoft.Rectangle bottomLeftEighth -dict-add keyCode -float 45 modifierFlags -float 1835008
+defaults write com.knollsoft.Rectangle bottomRightEighth -dict-add keyCode -float 44 modifierFlags -float 1835008
