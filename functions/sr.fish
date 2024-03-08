370612245d6cfe7ef2259cfe43e30af3bf7e069e
Mika Viljanen <mika.viljanen@wunderdog.fi>
2024-03-08 11:12:40 +0200

    Add optional command name to `sr` function

--- functions/sr.fish
+++ functions/sr.fish
@@ -1,7 +1,19 @@
-function sr
-	if test $status -eq 0
-		say -v Samantha 'Operation finished';
+function sr -a 'custom_message'
+	set success $status
+
+	if test -n "$custom_message"
+		set cmd_name $custom_message
 	else
-		say -v Samantha 'Operation failed';
+		set cmd_name "Operation"
 	end
+
+	if test $success -eq 0
+		set result "succeeded"
+	else
+		set result "failed"
+	end
+
+	say -v Samantha $cmd_name $result
+
+	return $success
 end
